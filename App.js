bf84e61abf55e346548c2c756804a45210c4bafb
David Crawford <crawford@mail.gvsu.edu>
2020-11-06 12:47:10 -0500

    Fixed android not requesting bluetooth permissions properly. Android should now display nearby devices.

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { SafeAreaView, View, Text, StatusBar } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, View, Text, StatusBar,PermissionsAndroid } from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
 
 import MainContainer from './src/components/MainContainer';
 export const bleManager = new BleManager();
 
 const App = () => {
+  useEffect(()=>{
+    (async () => {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permission Localisation Bluetooth',
+          message: 'Requirement for Bluetooth',
+          buttonNeutral: 'Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        }
+      );
+      const grantedCoarse = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        {
+          title: 'Permission Localisation Bluetooth',
+          message: 'Requirement for Bluetooth',
+          buttonNeutral: 'Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        }
+      );
+    })()
+  },[])
+  
   return (
     <>
       <StatusBar barStyle="light-content" />
